cb500ee44ba187d583e00726e188aded5d21dc9b
whalderman <halderman.warren@gmail.com>
2024-05-27 12:17:19 +0900

    up

--- init.lua
+++ init.lua
@@ -539,17 +539,17 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        gopls = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
+        tsserver = {},
+        svelte = {},
 
         lua_ls = {
           -- cmd = {...},
@@ -570,7 +570,7 @@ require('lazy').setup({
       -- Ensure the servers and tools above are installed
       --  To check the current status of installed tools and/or manually install
       --  other tools, you can run
-      --    :Mason
+      --
       --
       --  You can press `g?` for help in this menu.
       require('mason').setup()
