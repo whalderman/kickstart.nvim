cef485ccf705c69c488faf478c0675cb072aff9e
whalderman <halderman.warren@gmail.com>
2024-11-14 10:45:50 +0900

    add some lsp

--- init.lua
+++ init.lua
@@ -614,19 +614,13 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        biome = {}, -- biomejs
         -- clangd = {},
-        gopls = {},
-        pyright = {},
-        rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
-        svelte = {},
-
+        emmet_ls = {},
+        -- gopls = {},
+        html = {},
+        jsonls = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -641,6 +635,19 @@ require('lazy').setup({
             },
           },
         },
+        marksman = {}, -- markdown
+        -- phan = {}, -- php
+        -- protols = {}, -- protocol buffers
+        -- pyright = {},
+        rust_analyzer = {},
+        svelte = {},
+        -- Some languages (like typescript) have entire language plugins that can be useful:
+        --    https://github.com/pmizio/typescript-tools.nvim
+        --
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        ts_ls = {},
+        yamlls = {},
+        zls = {}, -- zig
       }
 
       -- Ensure the servers and tools above are installed
@@ -838,16 +845,19 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'ellisonleao/gruvbox.nvim',
+    'neanias/everforest-nvim',
+    version = false,
+    lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.o.background = 'dark'
+      vim.g.everforest_background = 'hard'
 
       -- You can configure highlights by doing something like:
-      vim.cmd [[colorscheme gruvbox]]
+      vim.cmd [[colorscheme everforest]]
     end,
   },
 
